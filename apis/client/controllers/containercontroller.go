16d55f65791fa50b0ac00be7aee243f225507c0e
FournyP <pierre.fourny76@gmail.com>
2022-10-07 18:01:04 +0200

    Simplify ContainerController

--- apis/client/controllers/containercontroller.go
+++ apis/client/controllers/containercontroller.go
@@ -2,23 +2,39 @@ package controllers
 
 import (
 	"context"
-	"flag"
+	"encoding/json"
 	"fmt"
-	"path/filepath"
+	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/rest"
 )
 
-type ContainerController struct{}
+type ContainerController struct {
+	kubernetesClient *kubernetes.Clientset
+}
 
 func NewContainerController() *ContainerController {
-	return &ContainerController{}
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// creates the kubernetesClient
+	kubernetesClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return &ContainerController{
+		kubernetesClient: kubernetesClient,
+	}
 }
 
 func (controller *ContainerController) CreateContainer(c *fiber.Ctx) error {
@@ -28,74 +44,48 @@ func (controller *ContainerController) CreateContainer(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).SendString("Please provide a name for the service")
 	}
 
-	createDeployment(name)
+	createDeployment(name, controller.kubernetesClient)
 
 	return c.SendString("Container created !")
 }
 
-func createDeployment(name string) {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+func renderTemplate(templatePath string, variables map[string]string) []byte {
+	data, err := os.ReadFile(templatePath)
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	deploymentsClient := clientset.AppsV1().Deployments(corev1.NamespaceDefault)
+	str := string(data)
 
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: int32Ptr(1),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "demo",
-				},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "demo",
-					},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "nginx",
-							Image: "nginx:1.19.4-alpine",
-							Ports: []corev1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      corev1.ProtocolTCP,
-									ContainerPort: 8080,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+	for key, element := range variables {
+		str = strings.Replace(str, "{{ "+key+" }}", element, -1)
+	}
+
+	return []byte(str)
+}
+
+func createDeployment(name string, kubernetesClient *kubernetes.Clientset) {
+	deploymentsClient := kubernetesClient.AppsV1().Deployments(corev1.NamespaceDefault)
+
+	variables := make(map[string]string)
+
+	variables["NAME"] = "test"
+	data := renderTemplate("./deployment.template.json", variables)
+
+	var deployment appsv1.Deployment
+
+	err := json.Unmarshal(data, &deployment)
+
+	if err != nil {
+		panic(err)
 	}
 
 	// Create Deployment
 	fmt.Println("Creating deployment...")
-	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	result, err := deploymentsClient.Create(context.TODO(), &deployment, metav1.CreateOptions{})
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
 }
-
-func int32Ptr(i int32) *int32 { return &i }
